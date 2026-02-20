f7952f5fa0beec582264fd30e6a47760ed1b234b
HIENLTH <hyhien@gmail.com>
2026-02-20 16:26:16 +0700

    Add project files.

---
+++ MiniShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
